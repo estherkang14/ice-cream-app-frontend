3aa8415a2a3616526f428c2970ac689e252124b9
mjester93 <m.jester.93@gmail.com>
2020-08-14 11:59:15 -0400

    store page

--- src/components/StorePage.js
+++ src/components/StorePage.js
@@ -14,10 +14,49 @@ class StorePage extends React.Component {
         .then(store => this.setState({store}))
     }
 
+    renderIceCreams = () => {
+        if (this.state.store['ice_creams']) {
+            return this.state.store['ice_creams'].map(
+                ic => { return (
+                    <li key={ic.id}>{ic.flavor} ({ic.calories} calories)</li>
+                )}
+            )
+        }
+    }
+
+    renderReviews = () => {
+        if (this.state.store.reviews && this.state.store.reviews.length > 0) {
+            return this.state.store.reviews.map(
+                review => { return (
+                    <div key={review.id} style={{paddingBottom: '20px'}}>
+                        <p>
+                            {review.user.username} - {review.user.location} ({review.rating}/5)
+                            <br />
+                            {review.text}
+                        </p>
+                    </div> 
+                )}
+            )
+        } else {
+            return <p>There are no reviews for this store!</p>
+        }
+    }
+
     render() {
         return (
-            <div>
+            <div className="ui left aligned container">
                 <h1>{this.state.store.name}</h1>
+                <h3>{this.state.store.location}</h3>
+                <div>
+                    <h4>Ice Cream Flavors: </h4>
+                    <ul>
+                        {this.renderIceCreams()}
+                    </ul>
+                </div>
+                <div>
+                    <h4>Reviews</h4>
+                    {this.renderReviews()}
+                </div>
             </div>
         )
     }
