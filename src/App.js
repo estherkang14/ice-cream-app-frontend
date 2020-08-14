3bdc3db196e17d46d694239bc89e107ae9ae19c3
mjester93 <m.jester.93@gmail.com>
2020-08-14 11:01:01 -0400

    sign up and stores links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import StoresContainer from './containers/StoresContainer';
 import NavBar from './components/NavBar';
+import SignUp from './components/SignUp';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 let BASEURL = "http://localhost:3000/"
 let STORESURL = BASEURL + "stores"
@@ -19,10 +21,17 @@ class App extends React.Component {
 
   render() { 
     return (
-      <div className="App">
-        <NavBar />
-        <StoresContainer stores={this.state.stores} /> 
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route path="/signup" render={(routeProps) => <SignUp {...routeProps} /> } />
+              <Route path="/" render={(routeProps) => <StoresContainer stores={this.state.stores} {...routeProps} /> } />
+            </Switch>
+          </div>
+        </div>
+      </ BrowserRouter>
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
-import SignUp from './SignUp';
 
 const NavBar = () => {
     return (
-        <BrowserRouter>
-            <div className="ui menu"> 
-                <a className="item">My Profile</a>
-                <a className="item">Stores</a>
-                <div className="right menu">
-                    <div className="item">
-                        <div className="ui button input">Log In</div>
-                    </div>
-                    <div className="item">
-                        <div className="ui button input">Log Out</div>
-                    </div>
-                    <div className="item">
-                        <div className="ui button input" ><Link to="/signup">Sign Up </Link>
-                        </div>
+        <div className="ui menu"> 
+            <a className="item">My Profile</a>
+            <a className="item" href="/">Stores</a>
+            <div className="right menu">
+                <div className="item">
+                    <div className="ui button input">Log In</div>
+                </div>
+                <div className="item">
+                    <div className="ui button input">Log Out</div>
+                </div>
+                <div className="item">
+                    <div className="ui button input" ><a href="/signup">Sign Up</a>
                     </div>
                 </div>
             </div>
-            <Switch>
-                <Route path="/signup" component={SignUp} />
-            </Switch>
-        
-        </BrowserRouter>
+        </div>
     )
 }
 
