319c289cdb4ea44eac65e099ea179bdc9d251112
estherkang14 <estherkang14@gmail.com>
2020-08-14 10:45:44 -0400

    arbitrary commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import SignUp from './SignUp';
 
 const NavBar = () => {
     return (
         <BrowserRouter>
-        <div className="ui menu"> 
-            <a className="item">My Profile</a>
-            <a className="item">Stores</a>
-            <div className="right menu">
-                <div className="item">
-                    <div className="ui button input">Log In</div>
-                </div>
-                <div className="item">
-                    <div className="ui button input">Log Out</div>
-                </div>
-                <div className="item">
-                    <div className="ui button input" ><Link to="/signup">Sign Up </Link>
+            <div className="ui menu"> 
+                <a className="item">My Profile</a>
+                <a className="item">Stores</a>
+                <div className="right menu">
+                    <div className="item">
+                        <div className="ui button input">Log In</div>
+                    </div>
+                    <div className="item">
+                        <div className="ui button input">Log Out</div>
+                    </div>
+                    <div className="item">
+                        <div className="ui button input" ><Link to="/signup">Sign Up </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Route path="/signup" component={SignUp} />
+            <Switch>
+                <Route path="/signup" component={SignUp} />
+            </Switch>
+        
         </BrowserRouter>
     )
 }
