167dcfa6983f23d9be0b690626a26c4ff8cb4aef
mjester93 <m.jester.93@gmail.com>
2020-08-18 11:00:06 -0400

    changing myprofile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const MyProfile = (props) => {
-    return (
-        <div></div>
-    )
+class MyProfile extends React.Component {
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div className="ui left aligned container">
+                <h1>My Profile</h1>
+    
+                <h2>Favorite Stores</h2>
+    
+                <h2>My Reviews</h2>
+            </div>
+        )
+    }
+   
 }
 
 export default MyProfile
