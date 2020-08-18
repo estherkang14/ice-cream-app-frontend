337a98f0f0aa207a88cedbcd0028282b9eb05bcf
mjester93 <m.jester.93@gmail.com>
2020-08-18 17:23:00 -0400

    css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,16 @@ const NavBar = (props) => {
     return (
         <div className="ui menu"> 
             {/* <a className="item" href="/my-profile">My Profile</a> */}
-            <a className="item" onClick={() => props.loginCheck()}>My Profile</a>
-            <a className="item" href="/">Stores</a>
+            <a className="item menu-item" onClick={() => props.loginCheck()}>My Profile</a>
+            <a className="item menu-item" href="/">Stores</a>
             <div className="right menu">
            
-                <div className="item">
-                    <div className="ui button input"><a href="/login">Log In</a></div>
+                <div className="item menu-item">
+                    <div className="ui button input"><a class="menu-item" href="/login">Log In</a></div>
                 </div> 
 
-                <div className="item">
-                    <div className="ui button input" ><a href="/signup">Sign Up</a></div>
+                <div className="item menu-item">
+                    <div className="ui button input" ><a class="menu-item" href="/signup">Sign Up</a></div>
                 </div>
             </div>
         </div>
--- src/components/NavBar2.js
+++ src/components/NavBar2.js
@@ -3,13 +3,13 @@ import React from 'react';
 const NavBar2 = (props) => {
     return (
         <div className="ui menu"> 
-            <a className="item" href="/my-profile">My Profile</a>
-            <a className="item" href="/">Stores</a>
+            <a className="item menu-item" href="/my-profile">My Profile</a>
+            <a className="item menu-item" href="/">Stores</a>
             <div className="right menu">
            
                
-                <div className="item">
-                    <div className="ui button input" onClick={(e) => {props.logOut(e)}}>Log Out</div>
+                <div className="item menu-item">
+                    <div className="ui button input menu-item" onClick={(e) => {props.logOut(e)}}>Log Out</div>
                 </div> 
               
             </div>
